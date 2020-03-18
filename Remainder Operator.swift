f48b0473e3078f128f7983e8c7c6102e72b406fa
Prouj <53300658+Prouj@users.noreply.github.com>
2020-03-18 20:24:06 -0300

    Create Remainder Operator.swift

---
+++ Remainder Operator.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+var num1: Int
+var num2: Float
+
+num1 = 9 % 4    // o resto segue o sial do dividendo 9 = (4 x 2) +1
+
+print(num1)
+
+
+num1 = -9 % 4   // -9 = (4 x -2) - 1 
+
+print(num1)
+
+num1 = 9 % -4 // Ignora o valor negativo do divisor, logo fica como na primeira operação
+
+print (num1)
+
+
+num2 = 9 / 1.5 //Desmonstração de uso do Float
+
+print(num2)
+
