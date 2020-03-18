f70e4a6215de842ffe83e8973b6063a912f81b73
Prouj <53300658+Prouj@users.noreply.github.com>
2020-03-18 20:28:02 -0300

    Create Typealias.swift

---
+++ Typealias.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+typealias 👍 = Int ; typealias 📓 = String //semicolons permite com o uso do ; escrever na mesma linha outros comandos
+
+
+var 😀: 👍, 🤣: 📓
+let 🐱‍👤 = 10
+let 😍 = 19
+
+😀 = 🐱‍👤 + 😍
+
+🤣 = " O valor de \(😀) é resultado da soma de \(🐱‍👤) com \(😍) "
+
+print(🤣)
