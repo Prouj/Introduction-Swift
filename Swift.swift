441666edfa4279d930de2beb1e6574f39f77e31f
Prouj <53300658+Prouj@users.noreply.github.com>
2020-03-29 23:12:09 -0300

    Create Swift.swift

---
+++ Swift.swift
@@ -0,0 +1,32 @@
+/* 
+375
+Swift is a new programming language for iOS, macOS, watchOS, and tvOS app development. 
+Nonetheless, many parts of Swift will be familiar from your experience of developing in C and Objective-C.
+Swift provides its own versions of all fundamental C and Objective-C types, including Int for integers, 
+Double and Float for floating-point values, Bool for Boolean values, and String for textual data.
+Swift also provides powerful versions of the three primary collection types, Array, Set, and Dictionary, 
+as described in Collection Types.
+Swift differs from other languages in that it is application-oriented, making it much more effective than
+other languages like ojective-C or Python.
+Swift defines away large classes of common programming errors by adopting modern programming patterns:
+
+Variables are always initialized before use.
+Array indices are checked for out-of-bounds errors.
+Integers are checked for overflow.
+Optionals ensure that nil values are handled explicitly.
+Memory is managed automatically.
+Error handling allows controlled recovery from unexpected failures.
+
+376
+A type-safe language, which means the language helps you to be clear about the types of values your code can work with.
+If part of your code requires a String, type safety prevents you from passing it an Int by mistake.
+Likewise, type safety prevents you from accidentally passing an optional String to a piece of code that 
+requires a non-optional String. Type safety helps you catch and fix errors as early as possible in the development process.
+
+377
+If you don’t specify the type of value you need, Swift uses type inference to work out the appropriate type.
+Type inference enables a compiler to deduce the type of a particular expression automatically when it
+compiles your code, simply by examining the values you provide.
+
+
+*/
